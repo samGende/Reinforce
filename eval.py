c4e8e391fca19dce965078f385dd491d59c68c68
samgende <sam.gende1415@gmail.com>
2025-03-30 11:34:12 -0500

    adjust eval to input model names

--- eval.py
+++ eval.py
@@ -2,14 +2,24 @@ from gsm8k import GSM8K_evaluation
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 from model_wrapper import TokenWrapper
+import argparse
+
+
+# gather args
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_name', type=str, required=True)
+args = parser.parse_args()  
+
+model_name = args.model_name
 
 evaluator = GSM8K_evaluation()
 
 # load model 
-
-model_name = 'meta-llama/Llama-3.2-1B-Instruct'  # Replace with your desired model variant
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
+
 model_wrapped = TokenWrapper(model, tokenizer)
+
+output_file = model_name.split("/")[-1] + "_results.json"
 # evaluate model
-acc, corrects = evaluator.eval(model_wrapped, n_evals=100, output_file="instruct_model_results.json")
+acc, corrects = evaluator.eval(model_wrapped, n_evals=1, output_file=output_file)
